5eb89a695550b1f0bf9064d64c7e10b4f6ffd0bf
hjkim <hjkim_sun@naver.com>
2025-01-07 14:52:38 +0900

    downloads.sh 추가

---
+++ ch11_8/downloads.sh
@@ -0,0 +1,6 @@
+DOWN_DIR=/home/ec2-user/downloads
+cd ${DOWN_DIR}
+curl -O https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.4/hadoop-aws-3.3.4.jar
+curl -O https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.769/aws-java-sdk-bundle-1.12.769.jar
+curl -O https://repo1.maven.org/maven2/software/amazon/awssdk/bundle/2.23.19/bundle-2.23.19.jar
+curl -O https://repo1.maven.org/maven2/org/wildfly/openssl/wildfly-openssl/1.1.3.Final/wildfly-openssl-1.1.3.Final.jar
