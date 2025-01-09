8174c6fa247d4c46c280896de813cf4124af0fe6
hjkim <hjkim_sun@naver.com>
2025-01-09 11:28:46 +0900

    완성

---
+++ ch12_2/downloads.sh
@@ -0,0 +1,7 @@
+DOWN_DIR=/home/ec2-user/downloads
+cd ${DOWN_DIR}
+curl -O https://repo1.maven.org/maven2/org/apache/spark/spark-streaming-kafka-0-10_2.12/2.4.0/spark-streaming-kafka-0-10_2.12-2.4.0.jar
+curl -O https://repo1.maven.org/maven2/org/apache/spark/spark-sql-kafka-0-10_2.12/3.5.2/spark-sql-kafka-0-10_2.12-3.5.2.jar
+curl -O curl -O https://repo1.maven.org/maven2/org/apache/kafka/kafka-clients/3.7.1/kafka-clients-3.7.1.jar
+curl -O curl -O https://repo1.maven.org/maven2/org/apache/spark/spark-token-provider-kafka-0-10_2.12/3.5.2/spark-token-provider-kafka-0-10_2.12-3.5.2.jar
+curl -O curl -O https://repo1.maven.org/maven2/org/apache/commons/commons-pool2/2.11.1/commons-pool2-2.11.1.jar
